9955d8065f83c7b6f9446402accd4c9797c7ca84
kcook769 <kevin@birchbox.com>
2019-11-17 09:45:38 -0500

    Removed un-used try:

--- python.py
+++ python.py
@@ -10,12 +10,11 @@ s = DHT22.sensor(pi, 4)
 f = open("templog.csv","a", 1)
 
 while True:
-  try:
-    s.trigger()
-    time.sleep(0.2)
-    temp = s.temperature() * 9/5.0 + 32 
-    now = datetime.datetime.now()
-    f.write ("{},{:.2f},{:.2f}\r\n".format(
-      now.strftime("%Y-%m-%d %H:%M:%S"),s.humidity(), temp ))
-    f.flush
-    time.sleep(59.8)
+  s.trigger()
+  time.sleep(0.2)
+  temp = s.temperature() * 9/5.0 + 32 
+  now = datetime.datetime.now()
+  f.write ("{},{:.2f},{:.2f}\r\n".format(
+    now.strftime("%Y-%m-%d %H:%M:%S"),s.humidity(), temp ))
+  f.flush
+  time.sleep(59.8)
